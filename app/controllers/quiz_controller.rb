0358c48d948a02e32030fdc6c0306c6f2401d4d5
Anand Patel <anand@Anands-iMac.local>
2021-01-29 15:41:49 -0500

    works

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,5 +1,5 @@
 class QuizController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   def index
     render json: Quiz.all
   end

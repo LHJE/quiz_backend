fa10b6a663a24f53d06589730bb7e8520d8a6307
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-02 09:22:09 -0700

    alternate db path

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,9 +1,10 @@
 class Quiz
-  if ENV["DATABASE_URL"]
-  PG.connect(ENV['DATABASE_URL'])
-elsif
-  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'quiz_backend_development'})
-end
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+    DB = PG.connect(host: "localhost", port: 5432, dbname: 'contact_development')
+  end
 
   def self.all
       results = DB.exec("SELECT * FROM quiz ORDER BY random() LIMIT 1;")
